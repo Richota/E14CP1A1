33ef4c89dfccc728a2e70015fd1fbd33cdf16fb9
RicardoSuarez <rsuarez.adv@gmail.com>
2018-09-11 22:15:41 -0300

    ADD:first commit

---
+++ Actividad27/app/controllers/products_controller.rb
@@ -0,0 +1,17 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to root_path
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
+end
